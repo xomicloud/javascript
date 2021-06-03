bfb96e167a5a0869c5befdc7aa983f05a35163d3
James Smith <james.smith@djalbat.com>
2021-06-03 10:53:52 +0100

    Small changes to the routers.

--- bin/router/restricted.js
+++ bin/router/restricted.js
@@ -3,14 +3,11 @@
 const express = require("express");
 
 const paths = require("../paths"),
-      signOutHandler = require("../handler/signOut"),
       homePageHandler = require("../handler/page/home");
 
-const { SIGN_OUT_PATH, HOME_PAGE_PATH } = paths;
+const { HOME_PAGE_PATH } = paths;
 
 const restrictedRouter = express.Router(); ///
-
-restrictedRouter.get(SIGN_OUT_PATH, signOutHandler);
 
 restrictedRouter.get(HOME_PAGE_PATH, homePageHandler);
 
--- bin/router/unrestricted.js
+++ bin/router/unrestricted.js
@@ -4,14 +4,17 @@ const express = require("express");
 
 const paths = require("../paths"),
       signInHandler = require("../handler/signIn"),
+      signOutHandler = require("../handler/signOut"),
       callbackHandler = require("../handler/callback"),
       createAccountHandler = require("../handler/createAccount");
 
-const { SIGN_IN_PATH, CALLBACK_PATH, CREATE_ACCOUNT_PATH } = paths;
+const { SIGN_IN_PATH, SIGN_OUT_PATH, CALLBACK_PATH, CREATE_ACCOUNT_PATH } = paths;
 
 const unrestrictedRouter = express.Router(); ///
 
 unrestrictedRouter.get(SIGN_IN_PATH, signInHandler);
+
+unrestrictedRouter.get(SIGN_OUT_PATH, signOutHandler);
 
 unrestrictedRouter.get(CALLBACK_PATH, callbackHandler);
 
