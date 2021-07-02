e7faa48a26c216bf8f54dda81379b823db1c99db
James Smith <james.smith@djalbat.com>
2021-07-02 18:15:54 +0100

    Removed the temporary local settings.

--- bin/options.js
+++ bin/options.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const { CLIENT_HOST, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env,
-      clientId = "1gH9BrXKHcJodD71", ///CLIENT_ID, ///
-      clientHost = "http://localhost:8001", ///CLIENT_HOST, ///
-      clientSecret = "yv5sHRwIFIlSbOE7bT4D5oSH0ImIMiHk", ///CLIENT_SECRET, ///
-      redirectURI = "http://localhost:8080/callback"; ///REDIRECT_URI; ///
+      clientId = CLIENT_ID, ///
+      clientHost = CLIENT_HOST, ///
+      clientSecret = CLIENT_SECRET, ///
+      redirectURI = REDIRECT_URI; ///
 
 module.exports = {
   clientId,
