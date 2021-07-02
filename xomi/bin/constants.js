83e69d7778100e655fda3980f5374f2852987779
James Smith <james.smith@djalbat.com>
2021-07-02 18:59:53 +0100

    Simplified the Google Drive Handler and put the API host back.

--- xomi/bin/constants.js
+++ xomi/bin/constants.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const POST_METHOD = "POST",
-      API_XOMI_CLOUD_HOST = "http://localhost:8005";  ///"https://api.xomi.cloud"
+      API_XOMI_CLOUD_HOST = "https://api.xomi.cloud";
 
 module.exports = {
   POST_METHOD,
